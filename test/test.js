2e97203238f54afc6754213cfb8f4b8bf5afd1eb
meng.zhang <meng.zhang.dev@ihandysoft.com>
2020-05-13 15:21:27 +0800

    init commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by meng.zhang on 5/13/20.
+ */
+
+var assert = require('assert')
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('Return -1 if element can not be searched', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(0))
+        })
+    })
+})
